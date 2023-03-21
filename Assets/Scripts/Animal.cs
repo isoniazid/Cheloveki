3a206c67ab9678ca6b30ccffaca38a461f4d3c6a
Vasya Ryzhkoff <ryzh.v.n@mail.ru>
2023-03-21 21:06:39 +0700

    Читернул

--- Assets/Scripts/Animal.cs
+++ Assets/Scripts/Animal.cs
@@ -1,18 +1,25 @@
 using UnityEngine;
 using System.Collections.Generic;
 using System;
-enum DIRECTIONS {FORWARD,BACKWARD,LEFT,RIGHT};
+
+public enum DIRECTIONS : int {
+    FORWARD = 0,
+    BACKWARD = 1,
+    LEFT = 2,
+    RIGHT = 3,
+    LEN
+    };
 
 public class Animal : MonoBehaviour
 {
 
-   private Dictionary<int,DIRECTIONS> DirDictionary = new Dictionary<int,DIRECTIONS>()
+/*    private Dictionary<int,DIRECTIONS> DirDictionary = new Dictionary<int,DIRECTIONS>()
     {
         {0, DIRECTIONS.FORWARD},
         {1, DIRECTIONS.BACKWARD},
         {2, DIRECTIONS.LEFT},
         {3, DIRECTIONS.RIGHT}
-    };
+    }; */
 //
 
     private float _step_size = 1f;
@@ -20,7 +27,7 @@ public class Animal : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        return;
     }
 
     void ChangePosition(Vector3 newCoords)
@@ -32,20 +39,20 @@ public class Animal : MonoBehaviour
     void Wobble()
     {
         var rnd = new System.Random();
-        int randomIndex = rnd.Next(DirDictionary.Count);
+        int randomIndex = rnd.Next((int)DIRECTIONS.LEN);
 
-        switch(DirDictionary[randomIndex])
+        switch(randomIndex)
         {
-            case DIRECTIONS.FORWARD:
+            case (int)DIRECTIONS.FORWARD:
             ChangePosition(new Vector3(0.0f,_step_size,0f));
             break;
-            case DIRECTIONS.BACKWARD:
+            case (int)DIRECTIONS.BACKWARD:
             ChangePosition(new Vector3(0.0f,-1f*_step_size,0f));
             break;
-            case DIRECTIONS.LEFT:
+            case (int)DIRECTIONS.LEFT:
             ChangePosition(new Vector3(-1f*_step_size,0.0f,0f));
             break;
-            case DIRECTIONS.RIGHT:
+            case (int)DIRECTIONS.RIGHT:
             ChangePosition(new Vector3(_step_size,0.0f,0f));
             break;
         }
